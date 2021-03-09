6594b8691f36c9980f243f53d78db43b515d4de7
TudorDan <tudor.ist@gmail.com>
2021-03-09 22:49:52 +0200

    Add initial domain class

--- ExamApp.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ExamApp.Domain
-{
-    public class Class1
-    {
-    }
-}
---
+++ ExamApp.Domain/Test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ExamApp.Domain
+{
+    public class Test
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
