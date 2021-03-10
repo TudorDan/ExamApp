5e8b72d8c21bddd4e4ef1d63942a8c0873e53ad9
TudorDan <tudor.ist@gmail.com>
2021-03-10 20:14:04 +0200

    Add mediator command handler for creating one exam

---
+++ ExamApp.Logic/Tests/Create.cs
@@ -0,0 +1,48 @@
+﻿using ExamApp.Data;
+using ExamApp.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExamApp.Logic.Tests
+{
+    public class Create
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var test = new Test
+                {
+                    Id = request.Id,
+                    Title = request.Title,
+                    Description = request.Description
+                };
+
+                _context.Tests.Add(test);
+                var success = await _context.SaveChangesAsync() > 0;
+
+                if (success) return Unit.Value;
+
+                throw new Exception("Problem saving changes");
+            }
+        }
+    }
+}
