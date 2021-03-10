2b8d4f19e290baef1ebff0a5dce988a92afe5bc2
TudorDan <tudor.ist@gmail.com>
2021-03-10 20:14:33 +0200

    Add POST controller for one exam

--- ExamApp.API/Controllers/TestsController.cs
+++ ExamApp.API/Controllers/TestsController.cs
@@ -32,5 +32,11 @@ namespace ExamApp.API.Controllers
         {
             return await _mediator.Send(new Article.Query { Id = id });
         }
+
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Create(Create.Command command)
+        {
+            return await _mediator.Send(command);
+        }
     }
 }
