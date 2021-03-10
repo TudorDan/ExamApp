939a7976017d56a99d7aaf01a0d39c84953ca0f2
TudorDan <tudor.ist@gmail.com>
2021-03-10 20:48:12 +0200

    Add mediator command handler for delete exam

---
+++ ExamApp.Logic/Tests/Delete.cs
@@ -0,0 +1,44 @@
+﻿using ExamApp.Data;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExamApp.Logic.Tests
+{
+    public class Delete
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var test = await _context.Tests.FindAsync(request.Id);
+
+                if (test == null)
+                    throw new Exception("Could not find test");
+
+                _context.Remove(test);
+
+                var success = await _context.SaveChangesAsync() > 0;
+
+                if (success) return Unit.Value;
+
+                throw new Exception("Problem saving changes");
+            }
+        }
+    }
+}
