65e85121e457af03523feec1aa89a0cda425f9a1
TudorDan <tudor.ist@gmail.com>
2021-03-10 20:33:46 +0200

    Add mediator command handler for editing exam

---
+++ ExamApp.Logic/Tests/Edit.cs
@@ -0,0 +1,47 @@
+﻿using ExamApp.Data;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExamApp.Logic.Tests
+{
+    public class Edit
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var test = await _context.Tests.FindAsync(request.Id);
+
+                if (test == null)
+                    throw new Exception("Could not find activity");
+
+                test.Title = request.Title ?? test.Title;
+                test.Description = request.Description ?? test.Description;
+
+                var success = await _context.SaveChangesAsync() > 0;
+
+                if (success) return Unit.Value;
+
+                throw new Exception("Problem saving changes");
+            }
+        }
+    }
+}
