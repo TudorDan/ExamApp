2f09f59adfd3b080cbea242b5cb6d1abfa018622
TudorDan <tudor.ist@gmail.com>
2021-03-19 22:05:04 +0200

    Add home page component

---
+++ ExamApp.UI/client-app/src/features/home/Home.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section id="hero">
+      <div className="hero-container" data-aos="zoom-in" data-aos-delay="100">
+        <h1 className="mb-4 pb-0">
+          Suitable
+          <br />
+          <span>Diverse</span> Tests
+        </h1>
+        <p className="mb-4 pb-0">Brevity is the soul of wit.</p>
+
+        <a href="#about" className="about-btn scrollto">
+          About the site
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
