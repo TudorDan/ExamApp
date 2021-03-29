71dd43c97364c5cdf3b2253d6684a435b4fb7e87
TudorDan <tudor.ist@gmail.com>
2021-03-29 16:14:08 +0300

    Refactor app component for mobx

--- ExamApp.UI/client-app/src/app/layout/App.tsx
+++ ExamApp.UI/client-app/src/app/layout/App.tsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
-import { ITest } from "../models/test";
+import { useEffect, useContext } from "react";
 import NavBar from "../../features/nav/NavBar";
 import Home from "../../features/home/Home";
 import About from "../../features/about/About";
@@ -7,95 +6,37 @@ import TestList from "../../features/list/TestList";
 import Footer from "../../features/footer/Footer";
 import TestDetails from "../../features/details/TestDetails";
 import TestForm from "../../features/form/TestForm";
-import agent from "../api/agent";
 import Loading from "./Loading";
 import TestStore from "../stores/testStore";
 import { observer } from "mobx-react-lite";
 
 const App = () => {
   const testStore = useContext(TestStore);
-  const [tests, setTests] = useState<ITest[]>([]);
-  const [selectedTest, setSelectedTest] = useState<ITest | null>(null);
-  const [editMode, setEditMode] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const [submitting, setSubmitting] = useState(false);
-
-  const handleSelectTest = (id: string) => {
-    setSelectedTest(tests.filter((t) => t.id === id)[0]);
-    setEditMode(false);
-  };
-
-  const handleOpenCreateForm = () => {
-    setSelectedTest(null);
-    setEditMode(true);
-  };
-
-  const handleCreateTest = (test: ITest) => {
-    setSubmitting(true);
-    agent.Tests.create(test)
-      .then(() => {
-        setTests([...tests, test]);
-        setSelectedTest(test);
-        setEditMode(false);
-      })
-      .then(() => setSubmitting(false));
-  };
-
-  const handleEditTest = (test: ITest) => {
-    setSubmitting(true);
-    agent.Tests.update(test)
-      .then(() => {
-        setTests([...tests.filter((t) => t.id !== test.id), test]);
-        setSelectedTest(test);
-        setEditMode(false);
-      })
-      .then(() => setSubmitting(false));
-  };
-
-  const handleDeleteTest = (id: string) => {
-    setSubmitting(true);
-    agent.Tests.delete(id)
-      .then(() => {
-        setTests([...tests.filter((t) => t.id !== id)]);
-      })
-      .then(() => setSubmitting(false));
-  };
+  const { editMode, selectedTest } = testStore;
 
   useEffect(() => {
     testStore.loadTests();
   }, [testStore]);
 
-  if (testStore.loadingInitial) return <Loading content="Loading Exams..." />;
-
-  if (submitting) return <Loading content="Loading..." />;
+  if (testStore.loadingInitial) return <Loading content="Loading Tests..." />;
 
   return (
     <>
-      <NavBar openCreateForm={handleOpenCreateForm} />
+      <NavBar />
 
       <Home />
 
       <main id="main">
         <About />
 
-        <TestList tests={testStore.tests} selectTest={handleSelectTest} />
+        <TestList />
 
-        {selectedTest && !editMode && (
-          <TestDetails
-            test={selectedTest}
-            setEditMode={setEditMode}
-            setSelectedTest={setSelectedTest}
-            deleteTest={handleDeleteTest}
-          />
-        )}
+        {selectedTest && !editMode && <TestDetails />}
 
         {editMode && (
           <TestForm
             key={selectedTest && (selectedTest.id || 0)}
-            setEditMode={setEditMode}
             test={selectedTest!}
-            createTest={handleCreateTest}
-            editTest={handleEditTest}
           />
         )}
       </main>
