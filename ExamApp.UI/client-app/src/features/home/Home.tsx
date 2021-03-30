cb751375b46b7effb526227a136410c909345c5d
TudorDan <tudor.ist@gmail.com>
2021-03-30 23:00:52 +0300

    Refactor home component

--- ExamApp.UI/client-app/src/features/home/Home.tsx
+++ ExamApp.UI/client-app/src/features/home/Home.tsx
@@ -1,18 +1,23 @@
 import React from "react";
+import About from "../about/About";
 
 const Home = () => {
   return (
-    <section id="hero">
-      <div className="hero-container" data-aos="zoom-in" data-aos-delay="100">
-        <h1 className="mb-4 pb-0">
-          Suitable
-          <br />
-          <span>Diverse</span> Tests
-        </h1>
+    <>
+      <section id="hero">
+        <div className="hero-container" data-aos="zoom-in" data-aos-delay="100">
+          <h1 className="mb-4 pb-0">
+            Suitable
+            <br />
+            <span>Diverse</span> Tests
+          </h1>
 
-        <p className="mb-4 pb-0">Brevity is the soul of wit.</p>
-      </div>
-    </section>
+          <p className="mb-4 pb-0">Brevity is the soul of wit.</p>
+        </div>
+      </section>
+
+      <About />
+    </>
   );
 };
 
