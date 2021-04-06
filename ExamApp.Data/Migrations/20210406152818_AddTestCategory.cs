14ea23c7bd98cd31948c45221304d0d61495a98a
TudorDan <tudor.ist@gmail.com>
2021-04-06 21:19:00 +0300

    Add test category migration

---
+++ ExamApp.Data/Migrations/20210406152818_AddTestCategory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ExamApp.Data.Migrations
+{
+    public partial class AddTestCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Tests",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Tests");
+        }
+    }
+}
