51c5fa4a03e2f7a1039debaf6712a7889eb2f3f6
TudorDan <tudor.ist@gmail.com>
2021-05-08 13:33:10 +0300

    Intercept 400 and 404 errors with axios

--- ExamApp.UI/client-app/src/app/api/agent.ts
+++ ExamApp.UI/client-app/src/app/api/agent.ts
@@ -1,18 +1,29 @@
 import axios, { AxiosResponse } from 'axios';
+import { history } from '../..';
 import { ITest } from '../models/test'
 
 axios.defaults.baseURL = 'http://localhost:34565/api';
 
+axios.interceptors.response.use(undefined, error => {
+  const { status, data, config } = error.response;
+  if (status === 404) {
+    history.push('/notfound');
+  }
+  if (status === 400 && config.method === 'get' && data.errors.hasOwnProperty('id')) {
+    history.push('/notfound');
+  }
+});
+
 const responseBody = (response: AxiosResponse) => response.data;
 
-const sleep = (ms: number) => (response: AxiosResponse) => 
+const sleep = (ms: number) => (response: AxiosResponse) =>
   new Promise<AxiosResponse>(resolve => setTimeout(() => resolve(response), ms));
 
 const requests = {
-    get: (url: string) => axios.get(url).then(sleep(1000)).then(responseBody),
-    post: (url: string, body: {}) => axios.post(url, body).then(sleep(1000)).then(responseBody),
-    put: (url: string, body: {}) => axios.put(url, body).then(sleep(1000)).then(responseBody),
-    del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody)
+  get: (url: string) => axios.get(url).then(sleep(1000)).then(responseBody),
+  post: (url: string, body: {}) => axios.post(url, body).then(sleep(1000)).then(responseBody),
+  put: (url: string, body: {}) => axios.put(url, body).then(sleep(1000)).then(responseBody),
+  del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody)
 }
 
 const Tests = {
