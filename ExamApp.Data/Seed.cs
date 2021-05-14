4bcb492c708a207e6662ba6b666a6a3c32493333
TudorDan <tudor.ist@gmail.com>
2021-05-14 23:21:22 +0300

    Add new db seed

--- ExamApp.Data/Seed.cs
+++ ExamApp.Data/Seed.cs
@@ -1,4 +1,5 @@
 ﻿using ExamApp.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -11,72 +12,83 @@ namespace ExamApp.Data
             if (!context.Tests.Any())
             {
                 var tests = new List<Test>
-                {                    
+                {
                     new Test
                     {
                         Title = "Erholungsmöglichkeiten, Ausbildungen für die Auswanderung",
                         Description = "Zertifikat Deutsch, B1",
-                        Category = "languages"
+                        Category = "languages",
+                        Creation = DateTime.Now.AddMonths(-2)
                     },                    
                     new Test
                     {
                         Title = "Integrale, Polinoame si Progresii",
                         Description = "Matematica, Clasa a XII-a",
-                        Category = "science"
+                        Category = "science",
+                        Creation = DateTime.Now.AddMonths(-3)
                     },
                     new Test
                     {
                         Title = "Structuri trofice in ecosisteme antropizate",
                         Description = "Biologie, Clasa a XII-a",
-                        Category = "science"
+                        Category = "science",
+                        Creation = DateTime.Now.AddDays(-55)
                     },                    
                     new Test
                     {
                         Title = "Derivati functionali ai acizilor carboxilici",
                         Description = "Chimie, Clasa a XII-a",
-                        Category = "science"
+                        Category = "science",
+                        Creation = DateTime.Now.AddDays(-50)
                     },
                     new Test
                     {
                         Title = "Simbolism, Modernism in textul poetic si proza narativa",
                         Description = "Limba Romana, Clasa a XII-a",
-                        Category = "humanities"
+                        Category = "humanities",
+                        Creation = DateTime.Now.AddMonths(-3)
                     },
                     new Test
                     {
                         Title = "Termodinamica si Electromagnetism",
                         Description = "Fizica, Clasa a XII-a",
-                        Category = "science"
+                        Category = "science",
+                        Creation = DateTime.Now.AddDays(-40)
                     },
                     new Test
                     {
                         Title = "Roci vulcanice",
                         Description = "Geografie, Clasa a X-a",
-                        Category = "humanities"
+                        Category = "humanities",
+                        Creation = DateTime.Now.AddDays(-35)
                     },
                     new Test
                     {
                         Title = "Provincia romana Dacia",
                         Description = "Istorie veche a romanilor",
-                        Category = "humanities"
+                        Category = "humanities",
+                        Creation = DateTime.Now.AddMonths(-1)
                     },
                     new Test
                     {
                         Title = "Chestionar auto nr. 1 / 2021",
                         Description = "Permise categoria B",
-                        Category = "diverse"
+                        Category = "diverse",
+                        Creation = DateTime.Now.AddDays(-27)
                     },
                     new Test
                     {
                         Title = "Test de inteligenta nr. 2 / 2021",
                         Description = "IQ",
-                        Category = "diverse"
+                        Category = "diverse",
+                        Creation = DateTime.Now.AddDays(-25)
                     },
                     new Test
                     {
                         Title = "Test de personalitate nr. 3 /2021",
                         Description = "Psihologie",
-                        Category = "personality"
+                        Category = "personality",
+                        Creation = DateTime.Now.AddDays(-20)
                     },
                 };
 
