5064294bd079d02ead47321d06388d5e14b12156
TudorDan <tudor.ist@gmail.com>
2021-06-04 23:56:29 +0300

    Add final form text area component

---
+++ ExamApp.UI/client-app/src/app/common/form/TextAreaInput.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { FieldRenderProps } from "react-final-form";
+
+interface IProps extends FieldRenderProps<string, any> {}
+
+const TextAreaInput: React.FC<IProps> = ({
+  input,
+  rows,
+  placeholder,
+  meta: { touched, error },
+}) => {
+  return (
+    <>
+      <textarea
+        rows={rows}
+        {...input}
+        placeholder={placeholder}
+        className="form-control"
+      />
+      {touched && !!error && <span>{error}</span>}
+    </>
+  );
+};
+
+export default TextAreaInput;
