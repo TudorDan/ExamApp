d6fe819730c6207b432b1574007539cd8360f234
TudorDan <tudor.ist@gmail.com>
2021-06-05 23:22:17 +0300

    Add  options from final form test select

---
+++ ExamApp.UI/client-app/src/app/common/options/categoryOptions.ts
@@ -0,0 +1,8 @@
+export const category = [
+    { key: '', text: 'Category', value: '' },
+    { key: 'languages', text: 'Languages', value: 'languages' },
+    { key: 'science', text: 'Science', value: 'science' },
+    { key: 'humanities', text: 'Humanities', value: 'humanities' },
+    { key: 'diverse', text: 'Diverse', value: 'diverse' },
+    { key: 'personality', text: 'Personality', value: 'personality' }
+];
