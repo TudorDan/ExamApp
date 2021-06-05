caaf4f9174758092d8d2d9660d89e7a6df4e3c41
TudorDan <tudor.ist@gmail.com>
2021-06-05 23:23:53 +0300

    Refactor TypeScript test interface for date type

--- ExamApp.UI/client-app/src/app/models/test.ts
+++ ExamApp.UI/client-app/src/app/models/test.ts
@@ -3,5 +3,25 @@
   title: string;
   description: string;
   category: string;
-  creation: string;
+  creation: Date;
+}
+
+export interface ITestFormValues extends Partial<ITest> {
+  time?: Date;
+}
+
+export class TestFormValues implements ITestFormValues {
+  id?: string = undefined;
+  title: string = '';
+  description: string = '';
+  category: string = '';
+  creation?: Date = undefined;
+  time?: Date = undefined;
+
+  constructor(init?: ITestFormValues) {
+    if (init && init.creation) {
+      init.time = init.creation;
+    }
+    Object.assign(this, init);
+  }
 }
