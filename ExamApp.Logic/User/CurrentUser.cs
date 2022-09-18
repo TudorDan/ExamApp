d0945e05cc09a674b30de49d7e0dc82a4f3d7e6a
TudorDan <tudor.ist@gmail.com>
2022-09-19 00:09:21 +0300

    Add Current User CQRS handler

---
+++ ExamApp.Logic/User/CurrentUser.cs
@@ -0,0 +1,42 @@
+﻿using ExamApp.Domain;
+using ExamApp.Logic.Interfaces;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExamApp.Logic.User
+{
+    public class CurrentUser
+    {
+        public class Query : IRequest<User> { }
+
+        public class Handler : IRequestHandler<Query, User>
+        {
+            private readonly UserManager<AppUser> _userManager;
+            private readonly IJWTGenerator _jwtGenerator;
+            private readonly IUserAccessor _userAccessor;
+
+            public Handler(UserManager<AppUser> userManager, IJWTGenerator jwtGenerator,
+                IUserAccessor userAccessor)
+            {
+                _userManager = userManager;
+                _jwtGenerator = jwtGenerator;
+                _userAccessor = userAccessor;
+            }
+
+            public async Task<User> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var user = await _userManager.FindByNameAsync(_userAccessor.GetCurrentUsername());
+
+                return new User
+                {
+                    DisplayName = user.DisplayName,
+                    Username = user.UserName,
+                    Token = _jwtGenerator.CreateToken(user),
+                    Image = null
+                };
+            }
+        }
+    }
+}
