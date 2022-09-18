dce01a8c1fa982185107ddaf4481956128a3346f
TudorDan <tudor.ist@gmail.com>
2022-09-18 23:35:04 +0300

    Add User domain class for Identity

--- ExamApp.Data/DataContext.cs
+++ ExamApp.Data/DataContext.cs
@@ -1,14 +1,20 @@
 ﻿using ExamApp.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ExamApp.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser>
     {
         public DataContext(DbContextOptions options) : base(options)
         {
         }
 
         public DbSet<Test> Tests { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
 }
