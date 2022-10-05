d200c6596cdea147c0e1a4d69aac510431750182
TudorDan <tudor.ist@gmail.com>
2022-10-06 00:17:33 +0300

    Update navbar for modals

--- ExamApp.UI/client-app/src/features/nav/NavBar.tsx
+++ ExamApp.UI/client-app/src/features/nav/NavBar.tsx
@@ -1,7 +1,10 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useContext } from "react";
 import { useScrollPosition } from "@n8tb1t/use-scroll-position";
 import { observer } from "mobx-react-lite";
 import { Link, NavLink } from "react-router-dom";
+import LoginForm from "../user/LoginForm";
+import { RootStoreContext } from "../../app/stores/rootStore";
+import RegisterForm from "../user/RegisterForm";
 
 const firstRender = window.location.href.split("/")[3] === "" ? true : false;
 
@@ -11,6 +14,8 @@ const NavBar: React.FC = () => {
   const [isRegister, SetIsRegister] = useState(false);
   const [isLogin, SetIsLogin] = useState(false);
   const headerContainerRef = useRef<HTMLDivElement>(null);
+  const rootStore = useContext(RootStoreContext);
+  const { isLoggedIn, user, logout } = rootStore.userStore;
 
   const handleToggle = (element: string) => {
     switch (element) {
@@ -27,16 +32,16 @@ const NavBar: React.FC = () => {
         SetIsLogin(false);
         break;
       case "register":
-        SetIsHome(false);
+        SetIsHome(true);
         SetIsTests(false);
-        SetIsRegister(true);
+        SetIsRegister(false);
         SetIsLogin(false);
         break;
       case "login":
-        SetIsHome(false);
+        SetIsHome(true);
         SetIsTests(false);
         SetIsRegister(false);
-        SetIsLogin(true);
+        SetIsLogin(false);
         break;
       default:
         SetIsHome(false);
@@ -69,70 +74,100 @@ const NavBar: React.FC = () => {
   });
 
   return (
-    <header
-      id="header"
-      className={`d-flex align-items-center ${isHome ? "" : "header-scrolled"}`}
-      ref={headerContainerRef}
-    >
-      <div className="container-fluid container-xxl d-flex align-items-center">
-        <div id="logo" className="me-auto">
-          <NavLink
-            to="/"
-            className="scrollto"
-            onClick={() => handleToggle("home")}
-          >
-            <img
-              src="assets/img/logo.png"
-              alt="logo"
-              title="personalisedLogo"
-            />
-          </NavLink>
+    <>
+      <header
+        id="header"
+        className={`d-flex align-items-center ${
+          isHome ? "" : "header-scrolled"
+        }`}
+        ref={headerContainerRef}
+      >
+        <div className="container-fluid container-xxl d-flex align-items-center">
+          <div id="logo" className="me-auto">
+            <NavLink
+              to="/"
+              className="scrollto"
+              onClick={() => handleToggle("home")}
+            >
+              <img
+                src="assets/img/logo.png"
+                alt="logo"
+                title="personalisedLogo"
+              />
+            </NavLink>
+          </div>
+
+          <nav id="navbar" className="navbar order-last order-lg-0">
+            <ul>
+              {isLoggedIn && user ? (
+                <>
+                  <li>
+                    <NavLink
+                      to="/tests"
+                      className={`nav-link scrollto ${isTests ? "active" : ""}`}
+                      onClick={() => handleToggle("tests")}
+                    >
+                      Tests
+                    </NavLink>
+                  </li>
+
+                  <li>
+                    <button id="logout-btn" onClick={logout}>
+                      Logout
+                    </button>
+                  </li>
+
+                  <li>
+                    <span>{`Welcome ${user.displayName}`}</span>
+                  </li>
+
+                  <Link to="/createTest">
+                    <button
+                      className="buy-tickets scrollto"
+                      onClick={() => handleToggle("")}
+                    >
+                      Create
+                    </button>
+                  </Link>
+                </>
+              ) : (
+                <>
+                  <li>
+                    <NavLink
+                      to={"/register"}
+                      className={`nav-link scrollto ${
+                        isRegister ? "active" : ""
+                      }`}
+                      onClick={() => handleToggle("register")}
+                      data-bs-toggle="modal"
+                      data-bs-target="#register-modal"
+                    >
+                      Register
+                    </NavLink>
+                  </li>
+
+                  <li>
+                    <NavLink
+                      to={"/login"}
+                      className={`nav-link me-5 scollto ${
+                        isLogin ? "active" : ""
+                      }`}
+                      onClick={() => handleToggle("login")}
+                      data-bs-toggle="modal"
+                      data-bs-target="#buy-ticket-modal"
+                    >
+                      Login
+                    </NavLink>
+                  </li>
+                </>
+              )}
+            </ul>
+          </nav>
         </div>
-
-        <nav id="navbar" className="navbar order-last order-lg-0">
-          <ul>
-            <li>
-              <NavLink
-                to="/tests"
-                className={`nav-link scrollto ${isTests ? "active" : ""}`}
-                onClick={() => handleToggle("tests")}
-              >
-                Tests
-              </NavLink>
-            </li>
-
-            <li>
-              <NavLink
-                to="/register"
-                className={`nav-link scrollto ${isRegister ? "active" : ""}`}
-                onClick={() => handleToggle("register")}
-              >
-                Register
-              </NavLink>
-            </li>
-
-            <li>
-              <NavLink
-                to="/login"
-                className={`nav-link scrollto ${isLogin ? "active" : ""}`}
-                onClick={() => handleToggle("login")}
-              >
-                Login
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-
-        <Link to="/createTest">
-          <button
-            className="buy-tickets scrollto"
-            onClick={() => handleToggle("")}
-          >
-            Create
-          </button>
-        </Link>
-      </div>
-    </header>
+      </header>
+      <RegisterForm />
+      <LoginForm />
+    </>
   );
 };
 
