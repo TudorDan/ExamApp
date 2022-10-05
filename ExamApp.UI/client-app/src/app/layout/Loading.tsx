34ae46b6dd2b2b73a53da618967ac9a83dd978e2
TudorDan <tudor.ist@gmail.com>
2022-10-06 00:20:24 +0300

    Fix Loading component not showing

--- ExamApp.UI/client-app/src/app/layout/Loading.tsx
+++ ExamApp.UI/client-app/src/app/layout/Loading.tsx
@@ -1,15 +1,30 @@
 import React from "react";
+import useWindowDimensions from "./useWindowDimensions";
 
 const Loading: React.FC<{
   content?: string;
   errorMessage?: string;
   sentMessage?: string;
 }> = ({ content, errorMessage, sentMessage }) => {
+  const { height } = useWindowDimensions();
+
   return (
-    <div id="loading" className="my-3">
-      <div className="loading">{content}</div>
-      {/* <div className="error-message">{errorMessage}</div>
-      <div className="sent-message">{sentMessage}</div> */}
+    <div id="loading" className="my-5" style={{ minHeight: height - 60 - 480 }}>
+      <div className="loading" style={{ display: content ? "block" : "none" }}>
+        {content}
+      </div>
+      <div
+        className="error-message"
+        style={{ display: errorMessage ? "block" : "none" }}
+      >
+        {errorMessage}
+      </div>
+      <div
+        className="sent-message"
+        style={{ display: sentMessage ? "block" : "none" }}
+      >
+        {sentMessage}
+      </div>
     </div>
   );
 };
