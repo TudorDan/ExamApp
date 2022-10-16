e5bca0c2df44298857d08c9ff0564d5eb588a623
TudorDan <tudor.ist@gmail.com>
2022-10-17 01:05:44 +0300

    Add domain model for questions

---
+++ ExamApp.Domain/Question.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExamApp.Domain
+{
+    public class Question
+    {
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+        public Guid TestId { get; set; }
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; }
+        public string Answer3 { get; set; }
+        public string Answer4 { get; set; }
+        public int CorrectAnswer { get; set; }
+    }
+}
