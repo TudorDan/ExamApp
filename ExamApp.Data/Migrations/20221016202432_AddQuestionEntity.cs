76d3b500379ce9fbb6ae68a8898e49c06ee8be91
TudorDan <tudor.ist@gmail.com>
2022-10-19 22:25:01 +0300

    Add ef core migration for questions table

---
+++ ExamApp.Data/Migrations/20221016202432_AddQuestionEntity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ExamApp.Data.Migrations
+{
+    public partial class AddQuestionEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Questions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Content = table.Column<string>(type: "TEXT", nullable: true),
+                    TestId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Answer1 = table.Column<string>(type: "TEXT", nullable: true),
+                    Answer2 = table.Column<string>(type: "TEXT", nullable: true),
+                    Answer3 = table.Column<string>(type: "TEXT", nullable: true),
+                    Answer4 = table.Column<string>(type: "TEXT", nullable: true),
+                    CorrectAnswer = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questions", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Questions");
+        }
+    }
+}
